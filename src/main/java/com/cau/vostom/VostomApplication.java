2ee34e356726decdc406d4405d2b27a73ea72b81
pjw-redt <pjiww4@gmail.com>
2023-10-25 15:05:30 +0900

    [init] Springboot 레포지토리 초기화 및 mySQL 설정 (#1)

---
+++ src/main/java/com/cau/vostom/VostomApplication.java
@@ -0,0 +1,13 @@
+package com.cau.vostom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VostomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VostomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cau/vostom/VostomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cau.vostom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VostomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
