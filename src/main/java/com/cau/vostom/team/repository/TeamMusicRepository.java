c40c225bf414bcbd4574ec88f3f0dbe6ad1a2353
pjw-redt <pjiww4@gmail.com>
2023-10-26 17:59:22 +0900

    [dev] TeamPackage Repository 구현(#4)

---
+++ src/main/java/com/cau/vostom/team/repository/TeamMusicRepository.java
@@ -0,0 +1,14 @@
+package com.cau.vostom.team.repository;
+
+import com.cau.vostom.team.domain.TeamMusic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeamMusicRepository extends JpaRepository<TeamMusic, Long>{
+
+    boolean existsByMusicIdAndTeamId(Long musicId, Long teamId);
+    List<TeamMusic> findByTeamId(Long teamId);
+
+    void deleteByMusicId(Long musicId);
+}
