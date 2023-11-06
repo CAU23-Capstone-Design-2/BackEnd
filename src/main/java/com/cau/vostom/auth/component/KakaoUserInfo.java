c3ef33e8e034d0c1abdb3f5e320a5667ba05e3b9
pjw-redt <pjiww4@gmail.com>
2023-11-06 20:31:47 +0900

    spring security 및 jwt secretkey

---
+++ src/main/java/com/cau/vostom/auth/component/KakaoUserInfo.java
@@ -0,0 +1,24 @@
+package com.cau.vostom.auth.component;
+
+import com.cau.vostom.auth.dto.KakaoUserInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@Component
+public class KakaoUserInfo { // 카카오 API를 이용해 토큰을 전송하여 유저 정보를 요청
+
+    private final WebClient webClient;
+    private static final String USER_INFO_URI = "https://kapi.kakao.com/v2/user/me";
+
+    public KakaoUserInfoResponse getUserInfo(String token) {
+        Flux<KakaoUserInfoResponse> response = webClient.get()
+                .uri(USER_INFO_URI)
+                .header("Authorization", "Bearer " + token)
+                .retrieve()
+                .bodyToFlux(KakaoUserInfoResponse.class);
+        return response.blockFirst();
+    }
+}
