7f953836c3efbf6f816fa0eac96f700f7c2800c0
pjw-redt <pjiww4@gmail.com>
2023-11-07 20:00:11 +0900

    [dev] userJwt 반환값 수정

--- src/main/java/com/cau/vostom/auth/controller/AuthController.java
+++ src/main/java/com/cau/vostom/auth/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.cau.vostom.auth.controller;
 
 import com.cau.vostom.auth.component.JwtTokenProvider;
+import com.cau.vostom.auth.dto.ResponseJwtDto;
 import com.cau.vostom.auth.service.KakaoAuthService;
-import com.cau.vostom.user.service.UserService;
 import com.cau.vostom.util.api.ApiResponse;
 import com.cau.vostom.util.api.ResponseCode;
 import lombok.RequiredArgsConstructor;
@@ -11,23 +11,20 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserService userService;
     private final KakaoAuthService kakaoAuthService;
     private final JwtTokenProvider jwtTokenProvider;
 
     // 카카오 로그인을 위해 회원가입 여부 확인, 이미 회원이면 Jwt 토큰 발급
     @PostMapping("/login")
-    public ApiResponse<HashMap<Long, String>> authCheck(@RequestHeader String accessToken) {
+    public ApiResponse<ResponseJwtDto> authCheck(@RequestHeader String accessToken) {
         Long userId = kakaoAuthService.userLogin(accessToken); // 유저 고유번호 추출
-        HashMap<Long, String> map = new HashMap<>();
-        map.put(userId, jwtTokenProvider.createToken(userId.toString()));
-        return ApiResponse.success(map, ResponseCode.USER_LOGINED.getMessage());
+        String userJwt = jwtTokenProvider.createToken(userId.toString());
+        return ApiResponse.success(ResponseJwtDto.of(userId, userJwt), ResponseCode.USER_LOGINED.getMessage());
     }
 }
