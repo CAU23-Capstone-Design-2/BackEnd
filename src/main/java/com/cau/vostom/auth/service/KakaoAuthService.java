dc8c603d1a306bd29797a639c8c65527825ac7f0
pjw-redt <pjiww4@gmail.com>
2023-11-09 12:36:00 +0900

    [fix] user kakao로그인&회원가입

--- src/main/java/com/cau/vostom/auth/service/KakaoAuthService.java
+++ src/main/java/com/cau/vostom/auth/service/KakaoAuthService.java
@@ -25,7 +25,7 @@ public class KakaoAuthService {
             return user.get().getId();
         }
         else {
-            User newUser = User.createUser(userInfo.getKakaoAccount().getProfile().getNickname(), userInfo.getKakaoAccount().getProfile().getProfileImageUrl(), userInfo.getId());
+            User newUser = User.createUser(userInfo.getKakao_account().getProfile().getNickname(), userInfo.getKakao_account().getProfile().getProfile_image_url(), userInfo.getId());
             return userRepository.save(newUser).getId();
         }
     }
