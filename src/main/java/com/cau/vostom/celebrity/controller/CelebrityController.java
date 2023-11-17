0cb96f01f4caea68373bf010413b3615f6eec2ef
pjw-redt <pjiww4@gmail.com>
2023-11-17 15:33:17 +0900

    celebritycontroller_연예인 리스트

---
+++ src/main/java/com/cau/vostom/celebrity/controller/CelebrityController.java
@@ -0,0 +1,32 @@
+package com.cau.vostom.celebrity.controller;
+
+import com.cau.vostom.celebrity.dto.response.ResponseCelebrityDto;
+import com.cau.vostom.celebrity.dto.response.ResponsePlayListDto;
+import com.cau.vostom.celebrity.service.CelebrityService;
+import com.cau.vostom.util.api.ApiResponse;
+import com.cau.vostom.util.api.ResponseCode;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = {"Celebrity"})
+@RequestMapping("/api/celebrity")
+public class CelebrityController {
+    private final CelebrityService celebrityService;
+
+    //연예인 리스트
+    @Operation(summary = "연예인 리스트")
+    @GetMapping("/playlist")
+    public ApiResponse<List<ResponseCelebrityDto>> getAllCelebrity() {
+        return ApiResponse.success(celebrityService.getCelebrityList(), ResponseCode.CELEBRITY_LISTED.getMessage());
+    }
+
+}
