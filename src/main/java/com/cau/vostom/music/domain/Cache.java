d68e704c6e7fbf6e2ffa7fe17419d69c282054d7
pjw-redt <pjiww4@gmail.com>
2023-12-01 19:16:03 +0900

    cache table과 cache Repository 구현

---
+++ src/main/java/com/cau/vostom/music/domain/Cache.java
@@ -0,0 +1,19 @@
+package com.cau.vostom.music.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cache {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cache_id")
+    private Long id;
+
+    private String youtubeCode;
+}
