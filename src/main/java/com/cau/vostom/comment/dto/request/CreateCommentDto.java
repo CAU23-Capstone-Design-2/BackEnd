7489ee48806b6efe77854bbd4c5b59cb5e9810cb
pjw-redt <pjiww4@gmail.com>
2023-12-02 19:21:15 +0900

    댓글 작성 기능에서 request userId 제거

--- src/main/java/com/cau/vostom/comment/dto/request/CreateCommentDto.java
+++ src/main/java/com/cau/vostom/comment/dto/request/CreateCommentDto.java
@@ -8,11 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCommentDto {
-    private Long userId;
     private Long coverSongId;
     private String content;
 
-    public static CreateCommentDto of(Long userId, Long coverSongId, String content) {
-        return new CreateCommentDto(userId, coverSongId, content);
+    public static CreateCommentDto of(Long coverSongId, String content) {
+        return new CreateCommentDto(coverSongId, content);
     }
 }
